3ce985377d138dba6019f84468c22cf1eea51505
Tim Shamilov <shamilovtim@gmail.com>
2023-04-22 23:23:57 -0400

    cleanup unneeded code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Provider as PaperProvider, MD3DarkTheme } from "react-native-paper";
 import { DarkTheme, NavigationContainer } from "@react-navigation/native";
 import { AppNavigator } from "./navigation/AppNavigator";
 import PolyfillCrypto from "react-native-webview-crypto";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const theme: typeof MD3DarkTheme = {
   ...MD3DarkTheme,
@@ -15,13 +14,11 @@ const theme: typeof MD3DarkTheme = {
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer theme={DarkTheme}>
-        <PaperProvider theme={theme}>
-          <PolyfillCrypto />
-          <AppNavigator />
-        </PaperProvider>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer theme={DarkTheme}>
+      <PaperProvider theme={theme}>
+        <PolyfillCrypto />
+        <AppNavigator />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
