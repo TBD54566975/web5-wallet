e682160221a256591f0cd171931f5fd0be0ffb10
Tim Shamilov <shamilovtim@gmail.com>
2023-04-26 15:46:03 -0400

    kebab case directories

--- actionextension.js
+++ actionextension.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from "expo";
 import { AppRegistry } from "react-native";
-import { ActionExtensionScreen } from "./src/features/action_extension/ActionExtensionScreen";
+import { ActionExtensionScreen } from "./src/features/action-extension/ActionExtensionScreen";
 
 AppRegistry.registerComponent("ActionExtension", () => ActionExtensionScreen);
