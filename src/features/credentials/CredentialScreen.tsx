9889507da2d5b41eccf1b2b1c7cc9952d1885593
Timothy Shamilov <shamilovtim@gmail.com>
2023-05-03 20:03:29 -0400

    add a basic credential list (#3)

--- src/features/credentials/CredentialScreen.tsx
+++ src/features/credentials/CredentialScreen.tsx
@@ -5,15 +5,50 @@ import { type Verifiable, type W3CCredential } from "verite";
 import { didStore } from "../profile/store";
 import { MockIssuerUtils } from "../mock-issuer/utils";
 import { StorageService } from "../../config/storageService";
+import { CredentialCard } from "./CredentialCard";
+import type { PrettyCredential } from "../../types/models";
 
 export const CredentialScreen = ({ route }) => {
-  const [credentials, setCredentials] = useState<Verifiable<W3CCredential>>();
+  const [credentials, setCredentials] = useState<Verifiable<W3CCredential>[]>(
+    []
+  );
+  const [prettyFields, setPrettyFields] = useState<PrettyCredential[]>([]);
 
   const onPressGetCredentials = async () => {
-    const issued = await MockIssuerUtils.issueCredentials(didStore?.didKey!);
-    setCredentials(issued);
-    StorageService.setObjectOrArray("credentials", issued);
+    const issuedCredentials = await MockIssuerUtils.issueCredentials(
+      didStore?.didKey!
+    );
+
+    setCredentials((previousCredentials) => [
+      ...previousCredentials,
+      issuedCredentials,
+    ]);
+
+    StorageService.setObjectOrArray("credentials", credentials);
+
+    // derive some nicely formatted data
+    const prettyFields = extractPrettyData(issuedCredentials);
+
+    if (prettyFields) {
+      setPrettyFields((previousFields) => [...previousFields, prettyFields]);
+    }
   };
+
+  const extractPrettyData = (credential: Verifiable<W3CCredential>) => {
+    for (const [key, values] of Object.entries(credential?.credentialSubject)) {
+      if (key !== "id") {
+        const type = values.type;
+        const date = new Date(values.approvalDate).toLocaleString();
+        const issuer =
+          Math.floor(Math.random() * 2) % 2 === 0
+            ? "Silicon Valley Bank"
+            : "FTX Inc.";
+        return { type, date, issuer };
+      }
+    }
+  };
+
+  const hasCredentials = !!credentials.length;
 
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
@@ -22,17 +57,22 @@ export const CredentialScreen = ({ route }) => {
           Welcome, {route.params.name + "\n\n"}
           Your DID ION is: {didStore.didIon + "\n\n"}
           Your DID Key is: {didStore?.didKey?.id + "\n\n"}
-          Click the below button to apply for credentials.
         </Text>
-        {!!credentials ? (
-          <Text variant="titleMedium">
-            Credentials are {JSON.stringify(credentials)}
-          </Text>
-        ) : (
-          <Button mode="contained" onPress={onPressGetCredentials}>
-            Get Credentials
-          </Button>
+        {hasCredentials && (
+          <>
+            {prettyFields?.map((cred, index) => (
+              <CredentialCard
+                key={index}
+                type={cred.type}
+                date={cred.date}
+                issuer={cred.issuer}
+              />
+            ))}
+          </>
         )}
+        <Button mode="contained" onPress={onPressGetCredentials}>
+          Apply for Credential
+        </Button>
       </View>
     </ScrollView>
   );
