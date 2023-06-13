8736f150de62f882c05b9528a6bd4d338db81b37
Tim Shamilov <shamilovtim@gmail.com>
2023-06-13 17:54:15 -0400

    add icons

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -5,14 +5,31 @@ import {
 } from "@react-navigation/bottom-tabs";
 import { SsiStackNavigator } from "./SsiStackNavigator";
 import { BrowserScreen } from "../features/browser/BrowserScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
     <Tab.Navigator screenOptions={TabNavigatorOptions}>
-      <Tab.Screen name="SSI" component={SsiStackNavigator} />
-      <Tab.Screen name="Browser" component={BrowserScreen} />
+      <Tab.Screen
+        name="SSI"
+        component={SsiStackNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="card-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Browser"
+        component={BrowserScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="globe-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
