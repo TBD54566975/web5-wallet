4f55593387495900b9b79ade23bb8e6a68b03df6
Adam Mika <88001738+amika-sq@users.noreply.github.com>
2023-06-22 15:35:09 -0600

    Use Web5 for Profile creation (#17)

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,12 +1,9 @@
-import type { DidKey, Verifiable, W3CCredential } from "verite";
+import type { Verifiable, W3CCredential } from "verite";
+import { Profile as Web5Profile } from "@tbd54566975/web5-user-agent";
 
 export type Credential = Verifiable<W3CCredential> & { id: string };
 
-export type Profile = {
-  id: string;
-  didKey: DidKey;
-  didIon: string;
-  name: string;
+export type Profile = Web5Profile & {
   credentials: Credential[];
 };
 
