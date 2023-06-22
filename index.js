dc3ff0ea68d511424d4d73cb7b9f18d06e74dcef
Tim Shamilov <shamilovtim@gmail.com>
2023-06-22 02:06:31 -0400

    fix polyfills

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import "./src/config/polyfills";
 import { registerRootComponent } from "expo";
-import "fastestsmallesttextencoderdecoder";
 
 import App from "./src/App";
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
