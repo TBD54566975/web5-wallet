e9699a5a88abf37c608c0fa53bf3978d8b4d1577
Adam Mika <amika@squareup.com>
2023-06-28 11:50:31 -0600

    Parse the URL params from deeplink route

--- src/features/deep-links/PermissionRequestScreen.tsx
+++ src/features/deep-links/PermissionRequestScreen.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { Text } from "react-native-paper";
 
-export const PermissionRequestScreen = () => {
+type Params = {
+  app: string;
+  "x-source": string;
+  "x-success": string;
+  "x-error": string;
+  req: string;
+};
+
+export const PermissionRequestScreen = ({ route }) => {
+  const params: Params = route.params;
+  console.log("app: " + params.app);
+  console.log("x-source: " + params["x-source"]);
+  console.log("x-success: " + params["x-success"]);
+  console.log("x-error: " + params["x-error"]);
+  console.log("req: " + params.req);
+
   return <Text>Hello, PermissionRequestScreen!</Text>;
 };
