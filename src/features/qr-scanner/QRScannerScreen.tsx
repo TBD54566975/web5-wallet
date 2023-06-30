b44fa14fb69a352ce8fab0aadc939858f2608913
Adam Mika <amika@squareup.com>
2023-06-30 07:44:14 -0600

    First pass - needs testing on device

--- src/features/qr-scanner/QRScannerScreen.tsx
+++ src/features/qr-scanner/QRScannerScreen.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export const QRScannerScreen = () => {
-  return <Text variant="headlineLarge">Hello, QRScannerScreen!</Text>;
+  const [hasPermission, setHasPermission] = useState<Boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+    getCameraPermissions();
+  }, []);
+
+  const handleScan = ({ type, data }) => {};
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  } else if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  } else {
+    return (
+      <SafeAreaView>
+        <BarCodeScanner
+          onBarCodeScanned={handleScan}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </SafeAreaView>
+    );
+  }
 };
