5a8c70f217912f071a09af4b1e5955c64f7ee90f
Adam Mika <amika@squareup.com>
2023-06-30 09:48:54 -0600

    Scan support

--- src/features/qr-scanner/QRScannerScreen.tsx
+++ src/features/qr-scanner/QRScannerScreen.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-import { Text } from "react-native-paper";
-import { BarCodeScanner } from "expo-barcode-scanner";
+import { Linking, StyleSheet, View } from "react-native";
+import { IconButton, Text } from "react-native-paper";
+import { BarCodeScanner, BarCodeScannerResult } from "expo-barcode-scanner";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { useLinkTo } from "@react-navigation/native";
 
-export const QRScannerScreen = () => {
+export const QRScannerScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState<Boolean | null>(null);
   const [scanned, setScanned] = useState(false);
+  const linkTo = useLinkTo();
 
   useEffect(() => {
     const getCameraPermissions = async () => {
@@ -15,7 +21,18 @@ export const QRScannerScreen = () => {
     getCameraPermissions();
   }, []);
 
-  const handleScan = ({ type, data }) => {};
+  const onPressClose = () => {
+    navigation.goBack();
+  };
+
+  const onBarCodeScanned = async ({ type, data }: BarCodeScannerResult) => {
+    setScanned(true);
+    const cleanedUrl = data.replace(/^web5:\/\//i, "/");
+    console.log("I see a qr code: " + data);
+    console.log("cleanedUrl: " + cleanedUrl);
+
+    linkTo(cleanedUrl);
+  };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -23,12 +40,25 @@ export const QRScannerScreen = () => {
     return <Text>No access to camera</Text>;
   } else {
     return (
-      <SafeAreaView>
+      <View style={styles.container}>
         <BarCodeScanner
-          onBarCodeScanned={handleScan}
+          onBarCodeScanned={scanned ? undefined : onBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
-        />
-      </SafeAreaView>
+        >
+          <SafeAreaView>
+            <IconButton icon="close" iconColor="white" onPress={onPressClose} />
+          </SafeAreaView>
+        </BarCodeScanner>
+      </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  closeButton: {
+    paddingTop: 10,
+  },
+});
