5bae2237e616529142c1275902aa69d26cb9f985
Adam Mika <amika@squareup.com>
2023-07-05 11:50:10 -0600

    Remove ActionExtension

--- actionextension.js
+++
@@ -1,7 +0,0 @@
-import "./src/config/polyfills";
-import "fastestsmallesttextencoderdecoder";
-import { AppRegistry } from "react-native";
-
-import { ActionExtensionEntry } from "./src/features/action-extension/ActionExtensionEntry";
-
-AppRegistry.registerComponent("ActionExtension", () => ActionExtensionEntry);
