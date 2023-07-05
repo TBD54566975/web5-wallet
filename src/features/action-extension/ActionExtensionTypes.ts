fd26867e9a3b76d7613146f9681bb9c54a1a853e
Adam Mika <88001738+amika-sq@users.noreply.github.com>
2023-07-05 14:26:11 -0600

    Remove ActionExtension (#28)

--- src/features/action-extension/ActionExtensionTypes.ts
+++
@@ -1,8 +0,0 @@
-import { PresentationDefinition as VeritePresentationDefinition } from "verite";
-
-export interface PresentationDefinition {
-  kind: "PresentationDefinition";
-  value: VeritePresentationDefinition;
-}
-
-export type WalletRequest = PresentationDefinition;
