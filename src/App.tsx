dad845297ed0fff8faf18c6bfccc268614281a62
Tim Shamilov <shamilovtim@gmail.com>
2023-09-06 12:34:06 -0400

    add back state bootstrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
+import { enableLegendStateReact } from "@legendapp/state/react";
 import { AppNavigator } from "./navigation/AppNavigator";
 import { DwnManger } from "./features/dwn/DwnManager";
 import { bootstrapIdentityAgent } from "./features/identity/identity-agent";
+
+enableLegendStateReact();
 
 const App = () => {
   useEffect(() => {
