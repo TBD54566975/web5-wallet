640341581549cd9e2b20056b3126663cc50498dd
Tim Shamilov <shamilovtim@gmail.com>
2023-09-06 12:37:48 -0400

    add back navigation provider at root

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React, { useEffect } from "react";
 import { enableLegendStateReact } from "@legendapp/state/react";
+import { type LinkingOptions } from "@react-navigation/native";
 import { AppNavigator } from "./navigation/AppNavigator";
 import { DwnManger } from "./features/dwn/DwnManager";
 import { bootstrapIdentityAgent } from "./features/identity/identity-agent";
+import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme } from "./theme/colors";
 
 enableLegendStateReact();
+
+const DeepLinkConfig: LinkingOptions<any> = {
+  prefixes: ["web5://"],
+  config: {
+    screens: {
+      PermissionRequestScreen: ":host/permission",
+    },
+  },
+};
 
 const App = () => {
   useEffect(() => {
@@ -19,7 +31,11 @@ const App = () => {
     void startupTasks();
   }, []);
 
-  return <AppNavigator />;
+  return (
+    <NavigationContainer linking={DeepLinkConfig} theme={DefaultTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import {
   type NativeStackNavigationOptions,
   createNativeStackNavigator,
@@ -18,9 +17,6 @@ import AuthScreen from "@/pages/onboarding/auth/Auth";
 import CreateScreen from "@/pages/onboarding/create/Create";
 import ImportScreen from "@/pages/onboarding/import/Import";
 
-import { type LinkingOptions } from "@react-navigation/native";
-import { DefaultTheme } from "@/theme/colors";
-
 import AddProfileScreen from "@/pages/default/profiles/AddProfile";
 import AddCredentialDetailScreen from "../pages/default/credentials/AddCredentialDetail";
 import AddCredentialOptionsScreen from "../pages/default/credentials/AddCredentialOptions";
@@ -29,63 +25,52 @@ const Stack = createNativeStackNavigator();
 
 const initialRoute = userProfiles.peek().length ? "Default" : "Onboarding";
 
-const DeepLinkConfig: LinkingOptions<any> = {
-  prefixes: ["web5://"],
-  config: {
-    screens: {
-      PermissionRequestScreen: ":host/permission",
-    },
-  },
-};
-
 export const AppNavigator = () => {
   return (
-    <NavigationContainer linking={DeepLinkConfig} theme={DefaultTheme}>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName={initialRoute}
-      >
-        {/* Onboarding */}
-        <Stack.Screen name="Onboarding" component={WelcomeScreen} />
-        <Stack.Screen name="Import" component={ImportScreen} />
-        <Stack.Screen name="Create" component={CreateScreen} />
-        <Stack.Screen name="Auth" component={AuthScreen} />
+    <Stack.Navigator
+      screenOptions={AppNavigatorOptions}
+      initialRouteName={initialRoute}
+    >
+      {/* Onboarding */}
+      <Stack.Screen name="Onboarding" component={WelcomeScreen} />
+      <Stack.Screen name="Import" component={ImportScreen} />
+      <Stack.Screen name="Create" component={CreateScreen} />
+      <Stack.Screen name="Auth" component={AuthScreen} />
 
-        {/* Default */}
-        <Stack.Screen name="Default" component={TabNavigator} />
-        <Stack.Group screenOptions={DefaultGroupOptions}>
-          <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
-          <Stack.Screen
-            name="CredentialDetail"
-            component={CredentialDetailScreen}
-          />
-          <Stack.Screen name="AddProfile" component={AddProfileScreen} />
-          <Stack.Screen
-            name="AddCredentials"
-            component={AddCredentialsScreen}
-          />
-          <Stack.Screen
-            name="AddCredentialDetail"
-            component={AddCredentialDetailScreen}
-          />
-          <Stack.Screen
-            name="AddCredentialOptions"
-            component={AddCredentialOptionsScreen}
-          />
-          <Stack.Screen
-            name="ConnectionDetail"
-            component={ConnectionDetailScreen}
-          />
-          <Stack.Screen
-            name="ReviewConnection"
-            component={ReviewConnectionScreen}
-          />
-        </Stack.Group>
-      </Stack.Navigator>
-    </NavigationContainer>
+      {/* Default */}
+      <Stack.Screen name="Default" component={TabNavigator} />
+      <Stack.Group screenOptions={DefaultGroupOptions}>
+        <Stack.Screen name="ProfileDetail" component={ProfileDetailScreen} />
+        <Stack.Screen
+          name="CredentialDetail"
+          component={CredentialDetailScreen}
+        />
+        <Stack.Screen name="AddProfile" component={AddProfileScreen} />
+        <Stack.Screen name="AddCredentials" component={AddCredentialsScreen} />
+        <Stack.Screen
+          name="AddCredentialDetail"
+          component={AddCredentialDetailScreen}
+        />
+        <Stack.Screen
+          name="AddCredentialOptions"
+          component={AddCredentialOptionsScreen}
+        />
+        <Stack.Screen
+          name="ConnectionDetail"
+          component={ConnectionDetailScreen}
+        />
+        <Stack.Screen
+          name="ReviewConnection"
+          component={ReviewConnectionScreen}
+        />
+      </Stack.Group>
+    </Stack.Navigator>
   );
 };
 
+const AppNavigatorOptions: NativeStackNavigationOptions = {
+  headerShown: false,
+};
 const DefaultGroupOptions: NativeStackNavigationOptions = {
   headerShown: true,
   headerTitle: () => false,
