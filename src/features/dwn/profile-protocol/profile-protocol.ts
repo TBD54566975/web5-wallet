4b2b32d066a79c8c8a813e608cf17d38dc97af44
Tim Shamilov <shamilovtim@gmail.com>
2023-09-14 12:37:38 -0400

    fix profile protocol based on feedback

--- src/features/dwn/profile-protocol/profile-protocol.ts
+++ src/features/dwn/profile-protocol/profile-protocol.ts
@@ -1,28 +1,21 @@
-import type { SafeOmit } from "@/types/utils";
-import type {
-  ProtocolRuleSet,
-  ProtocolDefinition,
-  ProtocolTypes,
-} from "@tbd54566975/dwn-sdk-js";
-
-// TODO: Not sure how to make this work yet. The upstream types don't have generics to accomodate type checking.
-export type ProfileProtocolType = {
-  displayName: string;
-};
-
-// TODO: Type definition of Protocols probably need to get fixed and the child types need to get exported:
-export type ProtocolDefinitionRefined = SafeOmit<
-  ProtocolDefinition,
-  "types" | "structure"
-> & {
-  types?: ProtocolTypes;
-  structure?: Record<string, ProtocolRuleSet>;
-};
-
-export const profileProtocol: ProtocolDefinitionRefined = {
-  // TODO: Needs to be fixed depending on what we do with Protocols: https://github.com/TBD54566975/web5-wallet/issues/66
+// TODO: Needs to be fixed depending on what we do with Protocols: https://github.com/TBD54566975/web5-wallet/issues/66
+export const profileProtocol = {
   protocol: "http://garfield.com/profile.schema.json",
-  types: undefined,
-  structure: undefined,
+  types: {
+    profile: {
+      schema: "http://garfield.com/profile.schema.json",
+      dataFormats: ["application/json"],
+    },
+  },
+  structure: {
+    profile: {
+      $actions: [
+        {
+          who: "anyone",
+          can: "read",
+        },
+      ],
+    },
+  },
   published: true,
 };
