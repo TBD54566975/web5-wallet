fbbe175f36db89eaafb0a469abb70b8a32b5485d
Tim Shamilov <shamilovtim@gmail.com>
2023-10-01 17:42:03 -0400

    mock permissionRequests

--- src/pages/default/connections/AddConnectionScreen.tsx
+++ src/pages/default/connections/AddConnectionScreen.tsx
@@ -10,6 +10,7 @@ import { base58btc } from "multiformats/bases/base58";
 import { base64url } from "multiformats/bases/base64";
 import { hkdf } from "@noble/hashes/hkdf";
 import { sha256 } from "@noble/hashes/sha256";
+import { xchacha20_poly1305 } from "@noble/ciphers/chacha";
 import { createJsonRpcRequest } from "@tbd54566975/web5-agent";
 import { FlexLayouts, Layouts } from "@/theme/layouts";
 import { useMount } from "@/hooks/useMount";
@@ -33,13 +34,13 @@ const AddConnectionScreen = ({ navigation }: Props) => {
     setIsScanned(true);
 
     // temporarily throw away the real data of whatever QR code that got scanned
-    console.log(data);
+    NOOP(data);
 
     // instead use a mock
     const mockQRContent =
-      "web5://connect?appDid=did%3Akey%3AzLCDinger&nonce=abc123&url=http%3A%2F%2Ffoobar.com%2Fdwn%2F";
+      "web5://connect?appDid=did%3Akey%3AzLCDinger&nonce=%5B0%2C%201%2C%202%2C%203%5D&url=http%3A%2F%2Ffoobar.com%2Fdwn%2F";
 
-    web5Connect(mockQRContent);
+    void web5Connect(mockQRContent);
   };
 
   const web5Connect = (qrContent: string) => {
@@ -47,11 +48,7 @@ const AddConnectionScreen = ({ navigation }: Props) => {
       // pull out the query params from the QR code
       const data = extractQueryParams(qrContent);
 
-      // requirement: derive the connect key
-      deriveConnectKey(data.connectDWADid);
-
-      // requirement: call out to connect (TODO)
-      sendConnect(data);
+      deriveConnectKey(data.dwaDID, data.connectNonce, data.serverURL);
     } catch (e: any) {
       alert(e.message);
       navigation.goBack();
@@ -61,39 +58,94 @@ const AddConnectionScreen = ({ navigation }: Props) => {
   const extractQueryParams = (qrContent: string) => {
     const { query } = queryString.parseUrl(qrContent);
 
-    const connectDWADid = query.appDid;
-    const connectNonce = query.nonce;
-    const connectURL = query.url;
-
     if (
-      typeof connectDWADid !== "string" ||
-      typeof connectNonce !== "string" ||
-      typeof connectURL !== "string"
+      typeof query.appDid !== "string" ||
+      typeof query.url !== "string" ||
+      typeof query.nonce !== "string"
     ) {
       throw new Error(
         "Received a malformed QR code. Please try scanning again."
       );
     }
 
-    return { connectDWADid, connectNonce, connectURL };
+    const dwaDID = query.appDid;
+    const serverURL = query.url;
+    const connectNonce = stringifiedArrayToUInt8Array(query.nonce);
+
+    if (!(connectNonce instanceof Uint8Array)) {
+      throw new Error(
+        "Not a valid connect nonce. Failed to create Uint8Array from it."
+      );
+    }
+
+    return { dwaDID, connectNonce, serverURL };
   };
 
   // derive Connect ID and Connect Key from the public key of the DWA
-  const deriveConnectKey = (dwaDid: string) => {
-    const appSignKeyId = dwaDid.substr(dwaDid.lastIndexOf(":") + 1);
-    const appSignPublicKey = base58btcMultibaseToBytes(appSignKeyId);
+  const deriveConnectKey = (
+    dwaDID: string,
+    connectNonce: Uint8Array,
+    serverURL: string
+  ) => {
+    const dwaSignKeyID = dwaDID.substring(dwaDID.lastIndexOf(":") + 1);
+    const dwaSignPublicKey = base58btcMultibaseToBytes(dwaSignKeyID);
 
-    // IDA derives the Connect UUID from the App's signing public key.
-    const uuidU8A = hkdf(
+    // derive the Connect UUID from the DWA's signing public key.
+    const connectUUID = hkdf(
       sha256,
-      appSignPublicKey,
+      dwaSignPublicKey,
       undefined,
       "connect_uuid",
       32
     );
-    const connectId = base64url.baseEncode(uuidU8A);
-    const requestId = uuid();
+    const connectId = base64url.baseEncode(connectUUID);
 
+    // derive the connect key from the DWA's public key
+    const connectKey = hkdf(
+      sha256,
+      dwaSignPublicKey,
+      undefined,
+      "connect_encryption_key",
+      32
+    );
+
+    console.log(dwaSignKeyID);
+    console.log(dwaSignPublicKey);
+    console.log(connectUUID);
+    console.log(connectId);
+
+    const connectRequestCipherText = fetchConnectRequestCipherText(
+      connectId,
+      serverURL
+    );
+
+    // decrypt connection request that came from Connect Server
+    const permissionRequests = decryptConnectRequest(
+      connectRequestCipherText,
+      connectKey,
+      connectNonce
+    );
+
+    // TODO: is the mock I'm returning the final shape of these
+    NOOP(permissionRequests);
+
+    // User selects the ION DID to use with the DWA.
+    // TODO: prompt UI? need designs.
+    // const connectedDid = "did:ion:EiCabc123";
+
+    // User authorizes the connection request, including requested permissions.
+    return { dwaSignPublicKey };
+  };
+
+  /**
+   * Reach out to connect server to get cipher text with connectRequest inside
+   */
+  const fetchConnectRequestCipherText = (
+    connectId: string,
+    connectURL: string
+  ) => {
+    // mock away the fetch for now
+    const requestId = uuid();
     const jsonRpcRequest = createJsonRpcRequest(
       requestId,
       "connect.retrieveRequest",
@@ -101,26 +153,52 @@ const AddConnectionScreen = ({ navigation }: Props) => {
         uuid: connectId,
       }
     );
-    const fetchOpts = {
-      method: "POST",
-      body: JSON.stringify(jsonRpcRequest),
-    };
+    NOOP(jsonRpcRequest);
+    NOOP(connectURL);
+    // const response = await fetch(connectURL, {
+    //   method: "POST",
+    //   body: JSON.stringify(jsonRpcRequest),
+    // });
+    // const { message: connectRequestCipherText } = await response.json();
 
-    console.log(appSignKeyId);
-    console.log(appSignPublicKey);
-    console.log(uuidU8A);
-    console.log(connectId);
+    // mock bc we dont fetch yet
+    const connectRequestCipherText = new Uint8Array([9, 9, 9]);
 
-    return "DerivedConnectKey";
+    return connectRequestCipherText;
   };
 
-  const sendConnect = (data: any) => {
-    console.log(data);
-  };
+  /**
+   * Create the cipher, decrypt the connectRequest, pull out permissions object
+   */
+  const decryptConnectRequest = (
+    cipherText: Uint8Array,
+    connectKey: Uint8Array,
+    connectNonce: Uint8Array
+  ) => {
+    // do nothing with the cipher and instead mock getting
+    // back a permission req because we dont fetch yet
+    const cipher = xchacha20_poly1305(connectKey, connectNonce);
+    NOOP(cipher);
+    // const decryptedConnectRequest = cipher.decrypt(cipherText).toString();
+    // const connectRequest = JSON.parse(decryptedConnectRequest);
+    // const { permissionsRequests } = connectRequest;
 
-  const base58btcMultibaseToBytes = (base58btcMultibase: string) => {
-    const multibaseBytes = base58btc.decode(base58btcMultibase);
-    return multibaseBytes.slice(2);
+    const mockPermissionsRequests = [
+      {
+        scope: {
+          protocol: "http://garfield.com/profile.schema.json",
+        },
+        conditions: {
+          delegation: false,
+          publication: false,
+          sharedAccess: true,
+          encryption: "required",
+          attestation: "prohibited",
+        },
+      },
+    ];
+
+    return mockPermissionsRequests;
   };
 
   // mock a valid scan
@@ -161,5 +239,28 @@ const styles = StyleSheet.create({
     height: 300,
   },
 });
+
+const base58btcMultibaseToBytes = (base58btcMultibase: string) => {
+  const multibaseBytes = base58btc.decode(base58btcMultibase);
+  return multibaseBytes.slice(2);
+};
+
+const stringifiedArrayToUInt8Array = (stringifiedArray: string) => {
+  try {
+    // Parse JSON string to get an array
+    const parsedArray = JSON.parse(stringifiedArray);
+
+    // Optional: Concatenate array elements
+    const concatenatedString = parsedArray.join(",");
+
+    // Encode the string to bytes
+    const utf8Bytes = new TextEncoder().encode(concatenatedString);
+
+    // Create and return a UInt8Array
+    return new Uint8Array(utf8Bytes);
+  } catch (e) {
+    return null;
+  }
+};
 
 export default AddConnectionScreen;
