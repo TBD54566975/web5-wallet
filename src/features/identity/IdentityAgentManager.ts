a2190c341397a1386e464a303bdd06cbcefadf2d
Adam Mika <amika@squareup.com>
2023-10-03 10:59:21 -0600

    Revert "Associate DWN endpoints with newly created ION DIDs"
    This reverts commit ee914546325ac8f7a496b737838161ff2a530eb2.

--- src/features/identity/IdentityAgentManager.ts
+++ src/features/identity/IdentityAgentManager.ts
@@ -6,8 +6,7 @@ import {
   DwnManager,
   SyncManagerLevel,
 } from "@web5/agent";
-import { getTechPreviewDwnEndpoints, Web5 } from "@web5/api";
-import { DidIonMethod, type DidIonCreateOptions } from "@web5/dids";
+import { Web5 } from "@web5/api";
 import {
   profileProtocol,
   Profile,
@@ -115,18 +114,9 @@ const createIdentity = async (
   didMethod: keyof CreateDidMethodOptions = "ion",
   kms = "local"
 ) => {
-  let didOptions: DidIonCreateOptions | undefined;
-  if (didMethod === "ion") {
-    const serviceEndpointNodes = await getTechPreviewDwnEndpoints();
-    didOptions = await DidIonMethod.generateDwnOptions({
-      serviceEndpointNodes,
-    });
-  }
-
   const identity = await agent.identityManager.create({
     name,
     didMethod,
-    didOptions,
     kms,
   });
 
