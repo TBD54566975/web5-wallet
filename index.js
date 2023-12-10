16f4ca13777a565efdf24e7cb6c5fb4e8061a3cb
Tim Shamilov <shamilovtim@gmail.com>
2023-12-10 14:06:59 -0500

    decouple from expo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "@tbd54566975/web5-react-native-polyfills";
 import "./src/utils/globals";
-import { registerRootComponent } from "expo";
+import { AppRegistry } from "react-native";
 import { polyfillBlob } from "./blob-polyfill";
 
 if (!global.structuredClone) {
@@ -13,4 +13,4 @@ import { App } from "./src/App";
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent("main", () => App);
