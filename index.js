c1cd8b0369248af877ce39b8f8e29ab035e723d0
Tim Shamilov <shamilovtim@gmail.com>
2023-12-15 19:15:58 -0500

    add subtle

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import "@tbd54566975/web5-react-native-polyfills";
 import "./src/utils/globals";
 import { AppRegistry } from "react-native";
 import { polyfillBlob } from "./blob-polyfill";
+import { Crypto } from "@peculiar/webcrypto";
 
 if (!global.structuredClone) {
   global.structuredClone = require("realistic-structured-clone");
 }
 
 polyfillBlob();
+
+global.crypto.subtle = new Crypto().subtle;
 
 import { App } from "./src/App";
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
