6106b38dfece64a9164e43b7dedda2f026fc30fe
Tim Shamilov <shamilovtim@gmail.com>
2024-01-12 13:43:28 -0500

    add checkable element

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,16 +3,16 @@ import { StyleSheet, View, type ViewStyle } from "react-native";
 import Octicons from "@expo/vector-icons/Octicons";
 import { SPACE } from "../theme/layouts";
 
-type Props = { style?: ViewStyle; checked: boolean };
-export const Checkbox = ({ style, checked }: Props) => {
+type Props = { style?: ViewStyle; checked: boolean; size?: number };
+export const Checkbox = ({ style, checked, size = 24 }: Props) => {
   const checkboxStyle = useMemo(
-    () => ({ ...styles.checkbox, ...style }),
-    [style]
+    () => ({ ...styles.checkbox, ...style, height: size, width: size }),
+    [size, style]
   );
 
   return (
     <View style={checkboxStyle}>
-      {!!checked && <Octicons name="check" size={16} />}
+      {!!checked && <Octicons name="check" size={size * 0.67} />}
     </View>
   );
 };
@@ -20,7 +20,7 @@ export const Checkbox = ({ style, checked }: Props) => {
 const styles = StyleSheet.create({
   checkbox: {
     padding: SPACE.XXSMALL,
-    borderWidth: 2,
+    borderWidth: 1,
     borderRadius: 2,
     height: SPACE.LARGE,
     width: SPACE.LARGE,
--- src/components/Tappable.tsx
+++ src/components/Tappable.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import Octicons from "@expo/vector-icons/Octicons";
 import { type ItemProps, Item } from "./Item";
+import { Checkbox } from "./Checkbox";
 
 type Props = { onPress: () => void } & ItemProps;
-export const Tappable = (props: Props) => {
-  const { source, iconName, badgeName, heading, subtitle, onPress } = props;
+export const Tappable = ({
+  source,
+  iconName,
+  badgeName,
+  heading,
+  subtitle,
+  onPress,
+}: Props) => {
   return (
     <Pressable style={styles.row} onPress={onPress}>
       <View style={styles.textContainer}>
@@ -19,6 +26,34 @@ export const Tappable = (props: Props) => {
       </View>
       <View style={styles.iconContainer}>
         <Octicons name="chevron-right" size={24} />
+      </View>
+    </Pressable>
+  );
+};
+
+type CheckableProps = { onPress: () => void; checked: boolean } & ItemProps;
+export const Checkable = ({
+  source,
+  iconName,
+  badgeName,
+  heading,
+  subtitle,
+  onPress,
+  checked = false,
+}: CheckableProps) => {
+  return (
+    <Pressable style={styles.row} onPress={onPress}>
+      <View style={styles.textContainer}>
+        <Item
+          iconName={iconName}
+          badgeName={badgeName}
+          source={source}
+          heading={heading}
+          subtitle={subtitle}
+        />
+      </View>
+      <View style={styles.iconContainer}>
+        <Checkbox checked={checked} size={16} />
       </View>
     </Pressable>
   );
