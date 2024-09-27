e397b2d57dac5a94ee1390bd3b9f94e52f781346
Tim Shamilov <shamilovtim@gmail.com>
2024-09-28 04:15:29 +0800

    disable console in release builds

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ import "@tbd54566975/web5-react-native-polyfills";
 import "./src/utils/globals";
 import "react-native-gesture-handler";
 import { AppRegistry, LogBox } from "react-native";
+import { App } from "./src/App";
 
 if (__DEV__) {
   LogBox.ignoreLogs([
     "`useBottomSheetDynamicSnapPoints` will be deprecated",
     "Provided value to SecureStore is larger than 2048 bytes. An attempt to store such a value will throw an error in SDK 35.",
-    "BarCodeScanner has been deprecated",
     "Sync failed: Error: Failed to dereference",
   ]);
 }
 
-import { App } from "./src/App";
+if (!__DEV__) disableConsole();
+
+const disableConsole = () => {
+  console.log = NOOP;
+  console.info = NOOP;
+  console.debug = NOOP;
+  console.warn = NOOP;
+  console.error = NOOP;
+};
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
